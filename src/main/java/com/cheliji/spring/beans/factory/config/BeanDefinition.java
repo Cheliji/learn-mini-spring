fea44196139f5750fbfdd28e093e8169e65b6370
bcnb <13550121+in-the-car-and@user.noreply.gitee.com>
2025-10-27 21:47:38 +0800

    为 Bean 填充属性

--- src/main/java/com/cheliji/spring/beans/factory/config/BeanDefinition.java
+++ src/main/java/com/cheliji/spring/beans/factory/config/BeanDefinition.java
@@ -1,5 +1,7 @@
 package com.cheliji.spring.beans.factory.config;
 
+import com.cheliji.spring.beans.PropertyValues;
+
 /**
  * Bean 的元数据，定义 Bean 的基本信息
  * 这里仅包含 class 类型
@@ -8,8 +10,15 @@ public class BeanDefinition {
 
     private Class beanClass;
 
+    private PropertyValues propertyValues;
+
     public BeanDefinition(Class beanClass) {
+        this(beanClass,null) ;
+    }
+
+    public BeanDefinition(Class beanClass, PropertyValues propertyValues) {
         this.beanClass = beanClass;
+        this.propertyValues = propertyValues;
     }
 
     public Class getBeanClass() {
@@ -18,6 +27,14 @@ public class BeanDefinition {
 
     public void setBeanClass(Class beanClass) {
         this.beanClass = beanClass;
+    }
+
+    public PropertyValues getPropertyValues() {
+        return propertyValues;
+    }
+
+    public void setPropertyValues(PropertyValues propertyValues) {
+        this.propertyValues = propertyValues;
     }
 
 }
--- src/test/java/com/cheliji/spring/test/factory/Dog.java
+++ src/test/java/com/cheliji/spring/test/factory/Dog.java
@@ -3,12 +3,21 @@ package com.cheliji.spring.test.factory;
 public class Dog {
 
 
+
     private String name ;
 
     private String color ;
 
     public void shout() {
         System.out.println("wangwang");
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                '}';
     }
 
 }
